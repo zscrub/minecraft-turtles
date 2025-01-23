04c92ac71453935742c892ccdc71178a70cc467f
Zack <rubinzack40@gmail.com>
2025-01-23 00:35:48 -0500

    added up and return

--- flat.lua
+++ flat.lua
@@ -1,7 +1,28 @@
 local x = 50
+local z = 0
 
-for i = 1, x do
-    if not turtle.forward() then
-        turtle.dig()
+local function dig_up_and_return()
+    while turtle.detectUp() do
+        turtle.digUp()
+        turtle.up()
+        z = z + 1
     end
+
+    while z > 0 do
+        turtle.down()
+        z = z - 1
+    end
+end
+
+for _ = 1, x do
+    for _ = 1, x do
+        dig_up_and_return()
+        if not turtle.forward() then
+            turtle.dig()
+        end
+    end
+    -- turn the turtle 180 degrees
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
 end
